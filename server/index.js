ac39511102d9e62b40ce2189bc60bb3af012a693
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-04 00:17:49 -0600

    Added method 2 route

--- server/index.js
+++ server/index.js
@@ -6,10 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { getCompliment, getFortune, getClassCompliments } = require('./controller')
+const { getCompliment, getFortune, getClassCompliments, deleteCompliment } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
 app.get("/api/fortune", getFortune);
 app.get("/api/compliments/classmates", getClassCompliments);
+app.delete("/api/compliments/classmates/:complimentId", deleteCompliment);
 
 app.listen(4000, () => console.log("Server running on 4000"));
