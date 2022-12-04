57979ceeaefbebc61cb87f6d75666a0336bce87b
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-03 23:31:35 -0600

    Added get method for method 1 to required controllers

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { getCompliment, getFortune } = require('./controller')
+const { getCompliment, getFortune, getClassCompliments } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
 app.get("/api/fortune", getFortune);
