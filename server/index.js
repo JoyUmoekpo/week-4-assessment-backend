55ddae32fd2d8a3eaf855a8bb20c13ab471d77b6
JoyUmoekpo <joyumoekpo1@gmail.com>
2022-12-03 23:00:09 -0600

    Formatted

--- server/index.js
+++ server/index.js
@@ -4,13 +4,11 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
 const { getCompliment, getFortune } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
-
 app.get("/api/fortune", getFortune);
 
 app.listen(4000, () => console.log("Server running on 4000"));
